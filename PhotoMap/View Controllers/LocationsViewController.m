a0f8c53a1f4900c6a52e04ad54b0f9679a6d8b0e
Emily Zheng <eazheng@fb.com>
2019-07-08 15:15:15 -0700

    progress

--- PhotoMap/View Controllers/LocationsViewController.m
+++ PhotoMap/View Controllers/LocationsViewController.m
@@ -8,9 +8,10 @@
 
 #import "LocationsViewController.h"
 #import "LocationCell.h"
+#import "PhotoMapViewController.h"
 
-static NSString * const clientID = @"YOUR_CLIENT_ID";
-static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
+static NSString * const clientID = @"QA1L0Z0ZNA2QVEEDHFPQWK0I5F1DE3GPLSNW4BZEBGJXUCFL";
+static NSString * const clientSecret = @"W2AOE1TYC4MHK5SZYOUGX0J3LVRALMPB4CXT3ZH21ZCPUMCU";
 
 @interface LocationsViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
 
@@ -29,6 +30,8 @@ static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     self.searchBar.delegate = self;
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,6 +45,7 @@ static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     LocationCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LocationCell" forIndexPath:indexPath];
+    [cell updateWithLocation:self.results[indexPath.row]];
     return cell;
 }
 
@@ -51,6 +55,10 @@ static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
     NSNumber *lat = [venue valueForKeyPath:@"location.lat"];
     NSNumber *lng = [venue valueForKeyPath:@"location.lng"];
     NSLog(@"%@, %@", lat, lng);
+    
+    [self.delegate locationsViewController:self didPickLocationWithLatitude:lat longitude:lng];
+    
+    
 }
 
 - (BOOL)searchBar:(UISearchBar *)searchBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
@@ -74,7 +82,13 @@ static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (data) {
-            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSString *jsonString = @"{ \"meta\": { \"code\": 200, \"requestId\": \"5ac51d7e6a607143d811cecb\" }, \"response\": { \"venues\": [ { \"id\": \"5642aef9498e51025cf4a7a5\", \"name\": \"Mr. Purple\", \"location\": { \"address\": \"180 Orchard St\", \"crossStreet\": \"btwn Houston & Stanton St\", \"lat\": 40.72173744277209, \"lng\": -73.98800687282996, \"labeledLatLngs\": [ { \"label\": \"display\", \"lat\": 40.72173744277209, \"lng\": -73.98800687282996 } ], \"distance\": 8, \"postalCode\": \"10002\", \"cc\": \"US\", \"city\": \"New York\", \"state\": \"NY\", \"country\": \"United States\", \"formattedAddress\": [ \"180 Orchard St (btwn Houston & Stanton St)\", \"New York, NY 10002\", \"United States\" ] }, \"categories\": [ { \"id\": \"4bf58dd8d48988d1d5941735\", \"name\": \"Hotel Bar\", \"pluralName\": \"Hotel Bars\", \"shortName\": \"Hotel Bar\", \"icon\": { \"prefix\": \"https:\/\/ss3.4sqi.net\/img\/categories_v2\/travel\/hotel_bar_\", \"suffix\": \".png\" }, \"primary\": true } ], \"venuePage\": { \"id\": \"150747252\" } } ] } }";
+            //NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSError *jsonError;
+            NSData *objectData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:objectData
+                                                                               options:NSJSONReadingMutableContainers
+                                                                                 error:&jsonError];
             NSLog(@"response: %@", responseDictionary);
             self.results = [responseDictionary valueForKeyPath:@"response.venues"];
             [self.tableView reloadData];
@@ -82,5 +96,7 @@ static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
     }];
     [task resume];
 }
+
+
 
 @end
--- PhotoMap/View Controllers/PhotoMapViewController.m
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "PhotoMapViewController.h"
+#import <MapKit/MapKit.h>
+#import "LocationsViewController.h"
 
-@interface PhotoMapViewController ()
+
+@interface PhotoMapViewController () < UIImagePickerControllerDelegate,UINavigationControllerDelegate, LocationsViewControllerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property UIImage *myImage;
 
 @end
 
@@ -17,7 +22,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+    
+
+
 }
+ 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -33,5 +46,72 @@
     // Pass the selected object to the new view controller.
 }
 */
+    
+- (IBAction)cameraButtonAction:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    // check if camera is available; if not, use photo
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+    
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    self.myImage = originalImage;
+    
+    // Do something with the images (based on your use case)
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+
+    [self dismissViewControllerAnimated:YES completion:nil];
+    [self performSegueWithIdentifier:@"tagSegue" sender:nil];
+}
+
+- (void)locationsViewController:(LocationsViewController *)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude {
+    [self.navigationController popToViewController:self animated:YES];
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = @"Picture!";
+    [self.mapView addAnnotation:annotation];
+    
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    if (annotationView == nil) {
+        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        annotationView.canShowCallout = true;
+        annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    }
+    
+    UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+    //imageView.image = [UIImage self.myImage]; //imageNamed:@"camera"];
+    imageView.image = [UIImage imageNamed:@"camera"];
+    return annotationView;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    LocationsViewController *locationViewController = segue.destinationViewController;
+    //set the delegate
+    locationViewController.delegate = self;
+    
+}
+
 
 @end
